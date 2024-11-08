6c0a023a12d8cf3c7ecf76d38e6fa0ee7587969c
siddhu <siddhuaug2498@gmail.com>
2024-11-08 10:13:55 +0530

    Initial commit

---
+++ projects/common/src/lib/common.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BlockCopyPasteDirective } from './directives/block-copy-paste.directive';
+
+
+
+@NgModule({
+  declarations: [
+    BlockCopyPasteDirective
+  ],
+  imports: [
+  ],
+  exports: [
+    BlockCopyPasteDirective
+  ]
+})
+export class CommonModule { }
